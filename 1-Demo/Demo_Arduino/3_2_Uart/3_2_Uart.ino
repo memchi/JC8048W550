473dac4b6f676e8e6e28b3fe9a3546c65f3467e7
Martin Emch <memch@gmx.ch>
2024-07-20 00:36:14 +0200

    english comments

--- 1-Demo/Demo_Arduino/3_2_Uart/3_2_Uart.ino
+++ 1-Demo/Demo_Arduino/3_2_Uart/3_2_Uart.ino
@@ -1,27 +1,27 @@
 /*
    Serial
-   串口通讯实验
+   Serial communication experiment
 */
 int incomedate = 0;
 void setup() {
 
-  Serial.begin(115200); //设置串口波特率115200
+  Serial.begin(115200); //Set the serial port baud rate 115200
 
-  Serial.println(78, BIN);// "1001110"
-  Serial.println(78, OCT);// "116"
-  Serial.println(78, DEC);// "78"
-  Serial.println(78, HEX);// "4E"
-  Serial.println(1.23456, 0);// "1"
-  Serial.println(1.23456, 2);// "1.23"
-  Serial.println(1.23456, 4);// "1.2346"
-  Serial.println('N');// "N"
-  Serial.println("Hello world.");// "Hello world."
+  Serial.println(78, BIN); // "1001110"
+  Serial.println(78, OCT); // "116"
+  Serial.println(78, DEC); // "78"
+  Serial.println(78, HEX); // "4E"
+  Serial.println(1.23456, 0); // "1"
+  Serial.println(1.23456, 2); // "1.23"
+  Serial.println(1.23456, 4); // "1.2346"
+  Serial.println('N'); // "N"
+  Serial.println("Hello world."); // "Hello world."
 }
 void loop() {
 
-  if (Serial.available() > 0)//串口接收到数据
+  if (Serial.available() > 0) // The serial port receives data
   {
-    incomedate = Serial.read();//获取串口接收到的数据
+    incomedate = Serial.read(); // Get the data received by the serial port
     if (incomedate == 'H')
     {
       Serial.println("I am OK!");
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 * Buy here: https://www.aliexpress.com/item/1005006715818797.html
 * Code base from here: http://pan.jczn1688.com/directlink/1/HMI%20display/JC8048W550.zip
+* Repo: https://github.com/memchi/JC8048W550
 
 ## Steps to make it running
-
 Commits: https://github.com/memchi/JC8048W550/commits/main/
