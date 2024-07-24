345f6524a09d1d53e3af567a578661ba58a54368
Martin Emch <memch@gmx.ch>
2024-07-24 20:40:05 +0200

    add some Serial.println

--- 1-Demo/Demo_Arduino/3_3-4_TFT-LVGL-Widgets/LvglWidgets/LvglWidgets.ino
+++ 1-Demo/Demo_Arduino/3_3-4_TFT-LVGL-Widgets/LvglWidgets/LvglWidgets.ino
@@ -120,15 +120,18 @@ void setup()
   // while (!Serial);
   Serial.println("LVGL Widgets Demo");
 
-  // Init touch device
-  
+  // Init touch device 
 
   // Init Display
   gfx->begin();
+  Serial.println("gfx->begin()");
 #ifdef TFT_BL
+  Serial.println("pinMode(TFT_BL, OUTPUT)");
   pinMode(TFT_BL, OUTPUT);
+  Serial.println("digitalWrite(TFT_BL, HIGH)");
   digitalWrite(TFT_BL, HIGH);
 #endif
+  Serial.println("gfx->fillScreen(XYZ)");
   gfx->fillScreen(RED);
   delay(500);
   gfx->fillScreen(GREEN);
@@ -137,27 +140,37 @@ void setup()
   delay(500);
   gfx->fillScreen(BLACK);
   delay(500);
+  Serial.println("lv_init()");
   lv_init();
   delay(10);
+  Serial.println("touch_init()");
   touch_init();
+  Serial.println("screenWidth = gfx->xyz()");
   screenWidth = gfx->width();
   screenHeight = gfx->height();
 #ifdef ESP32
+  Serial.println("ifdef ESP32: heap_caps_malloc");
   disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight/4 , MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
 #else
+  Serial.println("Not ESP32: malloc");
   disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight/4);
 #endif
+  Serial.println("if (!disp_draw_buf");
   if (!disp_draw_buf)
   {
     Serial.println("LVGL disp_draw_buf allocate failed!");
   }
   else
   {
+    Serial.println("lv_disp_draw_buf_init");
     lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight/4);
 
     /* Initialize the display */
+    Serial.println("Initialize the display");
     lv_disp_drv_init(&disp_drv);
+
     /* Change the following line to your display resolution */
+    Serial.println("Change the following line to your display resolution");
     disp_drv.hor_res = screenWidth;
     disp_drv.ver_res = screenHeight;
     disp_drv.flush_cb = my_disp_flush;
@@ -165,12 +178,14 @@ void setup()
     lv_disp_drv_register(&disp_drv);
 
     /* Initialize the (dummy) input device driver */
+    Serial.println("Initialize the (dummy) input device driver");
     static lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = my_touchpad_read;
     lv_indev_drv_register(&indev_drv);
 
+    Serial.println("lv_demo_widgets");
     lv_demo_widgets();
 
     Serial.println("Setup done");
